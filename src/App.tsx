2f6cde63f44a0c522be93b04038e4b8356cf98e3
yoojihyeon <maintain126@naver.com>
2022-04-25 23:14:54 +0900

    Feat

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Master <code>src/App.tsx</code> and save to reload.
+          Feat <code>src/App.tsx</code> Feat
         </p>
         <a
           className="App-link"
